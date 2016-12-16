7d799b88f9f200babc576ab07668e8c2be028faf
Pramod Shenoy <pramodhthegreats@gmail.com>
2016-12-16 23:46:33 +0530

    added first files via upload

---
+++ age_trial.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 16 17:52:20 2016
+
+@author: MAHE
+"""
+age=raw_input()
+days=int(age)*365
+print days
---
+++ expo_trial.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 16 17:27:16 2016
+
+@author: MAHE
+"""
+
+a=29809809850
+b=a/10e3
+print b
